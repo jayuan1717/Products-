a9d5c8c6bab47d9351ac285dcb014edd135cbe67
Jay Yuan <hayua7435@gmail.com>
2020-03-01 04:44:46 -0500

    final version

--- products.py
+++ products.py
@@ -4,8 +4,5 @@ while True:
     if name == 'q':
     	break
     price = input('Please entre the price:')
-    p = []
-    p.append(name)
-    p.append(price)
-    products.append(p)
+    products.append([name, price])
 print(products)
