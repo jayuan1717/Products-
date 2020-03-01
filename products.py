90406842a246d65d542080fd94d05950cd39fb35
Jay Yuan <hayua7435@gmail.com>
2020-03-01 04:34:39 -0500

    first version without checking video

---
+++ products.py
@@ -0,0 +1,8 @@
+products = []
+while True:
+    name = input('Please entre the product title: ')
+    if name == 'q':
+    	break
+    price = input('Please entre the price:')
+    products.append(name + price)
+print(products)
