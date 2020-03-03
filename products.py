c091f1479e518fe6452b0d4641f01ef4062f537d
Jay Yuan <hayua7435@gmail.com>
2020-03-03 04:25:27 -0500

    add read file

--- products.py
+++ products.py
@@ -1,11 +1,22 @@
 products = []
+
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if 'product, price' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, Price])
+print(products)
+
+# let user add new product and price
 while True:
     name = input('Please entre the product title: ')
     if name == 'q':
     	break
     price = input('Please entre the price:')
     products.append([name, price])
-#print(products)
+
+# print out the porduct and price 
 for p in products:
 	print(p[0], 'is selling at price of', p[1], 'CAD')
 
